235014292dee79ec5bff2f5b14a76f7e7df8af1a
bckps2 <bckfl10@gmail.com>
2023-04-11 01:37:55 +0200

    [Refactor01] - change connection and add Id of ProcessDto

--- App.InterViews.Report.Service/Dtos/ProcessDto.cs
+++ App.InterViews.Report.Service/Dtos/ProcessDto.cs
@@ -5,6 +5,7 @@ namespace App.InterViews.Report.Service.Dtos;
 
 public class ProcessDto
 {
+    public int IdProcess { get; set; }
     public string? JobPosition { get; set; }
     public int IdCompany { get; set; }
     public string? ExternalCompany { get; set; }
